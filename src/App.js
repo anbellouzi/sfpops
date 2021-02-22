efee21fc355c0816f1bf9ac138603e38b4c23a98
Anas Bellouzi <anbellouzi@gmail.com>
2021-02-21 23:08:34 -0500

    dynamic content and footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Title from './Title';
 import POPOSList from './POPOSList';
+import Footer from './Footer';
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <Title />
       <POPOSList />
+      <Footer />
 
     </div>
   );
---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+// src/Project.js
+
+import React from 'react'
+import './Footer.css';
+
+function Footer() {
+    return (
+      <div className="Footer">
+        <h2>Mitchell Hudson copyright 2019</h2>
+      </div>
+    );
+  }
+
+export default Footer
